2abfe2ff68935abe3066619683e43a0ee478218a
alexeispirit <alexeispirit@gmail.com>
2019-11-25 17:05:32 +0500

    215-Extracting-selection-logic

--- 04-movie-fight/index.js
+++ 04-movie-fight/index.js
@@ -20,6 +20,12 @@ createAutoComplete({
       <img src="${imgSrc}" />
       ${movie.Title} (${movie.Year})
     `;
+  },
+  onOptionSelect(movie) {
+    onMovieSelect(movie);
+  },
+  inputValue(movie) {
+    return movie.Title;
   }
 });
 
