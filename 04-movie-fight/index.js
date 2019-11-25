63d36ab30ad5129a7808f1ec1133fae9099f6d14
alexeispirit <alexeispirit@gmail.com>
2019-11-25 16:30:56 +0500

    208-making-followup-request

--- 04-movie-fight/index.js
+++ 04-movie-fight/index.js
@@ -49,6 +49,7 @@ const onInput = async event => {
     option.addEventListener("click", () => {
       dropdown.classList.remove("is-active");
       input.value = movie.Title;
+      onMovieSelect(movie);
     });
 
     resultsWrapper.appendChild(option);
@@ -62,3 +63,14 @@ document.addEventListener("click", event => {
     dropdown.classList.remove("is-active");
   }
 });
+
+const onMovieSelect = async movie => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "c6d3292d",
+      i: movie.imdbID
+    }
+  });
+
+  return response.data;
+};
