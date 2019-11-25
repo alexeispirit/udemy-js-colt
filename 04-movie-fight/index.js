6315c10cf2652c74367c88e6b1295464ff74f6b2
alexeispirit <alexeispirit@gmail.com>
2019-11-25 17:22:31 +0500

    221-showing-two-summaries

--- 04-movie-fight/index.js
+++ 04-movie-fight/index.js
@@ -6,10 +6,6 @@ const autoCompleteConfig = {
       ${movie.Title} (${movie.Year})
     `;
   },
-  onOptionSelect(movie) {
-    document.querySelector(".tutorial").classList.add("is-hidden");
-    onMovieSelect(movie);
-  },
   inputValue(movie) {
     return movie.Title;
   },
@@ -30,15 +26,23 @@ const autoCompleteConfig = {
 
 createAutoComplete({
   ...autoCompleteConfig,
-  root: document.querySelector("#left-autocomplete")
+  root: document.querySelector("#left-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#left-summary"));
+  }
 });
 
 createAutoComplete({
   ...autoCompleteConfig,
-  root: document.querySelector("#right-autocomplete")
+  root: document.querySelector("#right-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#right-summary"));
+  }
 });
 
-const onMovieSelect = async movie => {
+const onMovieSelect = async (movie, summaryElement) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "c6d3292d",
@@ -46,7 +50,7 @@ const onMovieSelect = async movie => {
     }
   });
 
-  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = movieDetail => {
