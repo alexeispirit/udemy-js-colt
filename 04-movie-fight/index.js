91d2f1997e89a6846da172f7a5fffcb7b3eb01e6
alexeispirit <alexeispirit@gmail.com>
2019-11-25 15:58:01 +0500

    198-rendering-movies

--- 04-movie-fight/index.js
+++ 04-movie-fight/index.js
@@ -13,7 +13,16 @@ const input = document.querySelector("input");
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
-  console.log(movies);
+
+  const target = document.querySelector("#target");
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+    target.appendChild(div);
+  }
 };
 
 input.addEventListener("input", debounce(onInput));
