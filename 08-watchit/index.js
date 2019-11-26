85b3ad23677b5c2b21b08efee495931717043e5e
alexeispirit <alexeispirit@gmail.com>
2019-11-26 14:39:29 +0500

    303-watching-files-with-chokidar

--- 08-watchit/index.js
+++ 08-watchit/index.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+const chokidar = require("chokidar");
+
+chokidar
+  .watch(".")
+  .on("add", () => console.log("file added"))
+  .on("change", () => console.log("file changed"))
+  .on("unlink", () => console.log("file unlinked"));
