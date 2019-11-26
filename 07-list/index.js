2fc2e709dc1a08c37c6f8c916385f5eb667a3dee
alexeispirit <alexeispirit@gmail.com>
2019-11-26 14:18:58 +0500

    295-promise-all-based-solution

--- 07-list/index.js
+++ 07-list/index.js
@@ -2,11 +2,20 @@
 // use npm link to link
 
 const fs = require("fs");
+const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
-  console.log(filenames);
+  const statPromises = filenames.map(filename => lstat(filename));
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    console.log(filenames[index], stats.isFile());
+  }
 });
