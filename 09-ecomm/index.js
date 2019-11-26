97246e23618b5e054f90ef1f8186e9fbaf96ae8d
alexeispirit <alexeispirit@gmail.com>
2019-11-26 16:58:45 +0500

    322-globally-applying-middleware

--- 09-ecomm/index.js
+++ 09-ecomm/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send(`
     <div>
       <form method="POST">
-        <input placeholder="email" />
-        <input placeholder="password" />
-        <input placeholder="password confirmation" />
+        <input placeholder="email" name="email" />
+        <input placeholder="password" name="password" />
+        <input placeholder="password confirmation" name="password confirmation" />
         <button>Sign Up</button>
       </form>
     </div>
@@ -16,6 +19,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
+  console.log(req.body);
   res.send("account created");
 });
 
