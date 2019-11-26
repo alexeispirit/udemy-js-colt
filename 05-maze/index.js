53856caa48c8b004de51b5d7bd5be90b5bfa7608
alexeispirit <alexeispirit@gmail.com>
2019-11-26 11:08:19 +0500

    261-understanding-new-unit-variables

--- 05-maze/index.js
+++ 05-maze/index.js
@@ -1,10 +1,14 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
 const cells = 3;
+const cellsHorizontal = 4;
+const cellsVertical = 3;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 const unitLength = width / cells;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
 
 const engine = Engine.create();
 engine.world.gravity.y = 0;
