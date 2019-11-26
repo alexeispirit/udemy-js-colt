de8a6e0bb9a06e06fcd46e6275018e7ee2a6f3cb
alexeispirit <alexeispirit@gmail.com>
2019-11-26 15:45:27 +0500

    316-creating-web-server

--- 09-ecomm/index.js
+++ 09-ecomm/index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hi");
+});
+
+app.listen(3000, () => {
+  console.log("listening");
+});
