cedee25dd3eeb595845a798bfc730ba1be6dd173
alexeispirit <alexeispirit@gmail.com>
2019-11-27 14:33:32 +0500

    377-reusable-error-handling-middleware

--- 09-ecomm/routes/admin/auth.js
+++ 09-ecomm/routes/admin/auth.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
 
 const usersRepo = require("../../repositories/users");
 
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
+const { handleErrors } = require("./middlewares");
 
 const {
   requireEmail,
@@ -23,13 +23,8 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
     const { email, password } = req.body;
     const user = await usersRepo.create({ email, password });
 
@@ -51,13 +46,8 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
 
