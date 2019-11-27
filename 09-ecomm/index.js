63b29cdbd3c59eb75857624009c7815f0477d3ba
alexeispirit <alexeispirit@gmail.com>
2019-11-27 08:57:38 +0500

    336-signup-validation-logic

--- 09-ecomm/index.js
+++ 09-ecomm/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const usersRepo = require("./repositories/users");
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,15 +13,25 @@ app.get("/", (req, res) => {
       <form method="POST">
         <input placeholder="email" name="email" />
         <input placeholder="password" name="password" />
-        <input placeholder="password confirmation" name="password confirmation" />
+        <input placeholder="password confirmation" name="passwordConfirmation" />
         <button>Sign Up</button>
       </form>
     </div>
   `);
 });
 
-app.post("/", (req, res) => {
-  console.log(req.body);
+app.post("/", async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send("Email in use");
+  }
+
+  if (password !== passwordConfirmation) {
+    return res.send("Passwords must match");
+  }
+
   res.send("account created");
 });
 
