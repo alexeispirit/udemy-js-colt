22387a3d863f52df4d6f0c2a26e7924f6b57ab33
alexeispirit <alexeispirit@gmail.com>
2019-11-27 11:38:49 +0500

    360-template-helper-functions

--- 09-ecomm/views/admin/auth/signin.js
+++ 09-ecomm/views/admin/auth/signin.js
@@ -1,12 +1,15 @@
 const layout = require("../layout");
+const { getError } = require("../../helpers");
 
-module.exports = () => {
+module.exports = ({ errors }) => {
   return layout({
     content: `
       <div>
         <form method="POST">
           <input placeholder="email" name="email" />
+          ${getError(errors, "email")}
           <input placeholder="password" name="password" />
+          ${getError(errors, "password")}
           <button>Sign In</button>
         </form>
       </div>    
