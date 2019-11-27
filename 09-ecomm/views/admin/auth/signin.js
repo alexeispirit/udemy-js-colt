84f236fbc98115f3737ac06e90c6a006676e2ee2
alexeispirit <alexeispirit@gmail.com>
2019-11-27 10:45:28 +0500

    351-building-layout-file

--- 09-ecomm/views/admin/auth/signin.js
+++ 09-ecomm/views/admin/auth/signin.js
@@ -1,11 +1,15 @@
+const layout = require("../layout");
+
 module.exports = () => {
-  return `
-    <div>
-      <form method="POST">
-        <input placeholder="email" name="email" />
-        <input placeholder="password" name="password" />
-        <button>Sign In</button>
-      </form>
-    </div>    
-  `;
+  return layout({
+    content: `
+      <div>
+        <form method="POST">
+          <input placeholder="email" name="email" />
+          <input placeholder="password" name="password" />
+          <button>Sign In</button>
+        </form>
+      </div>    
+    `
+  });
 };
--- 09-ecomm/views/admin/auth/signup.js
+++ 09-ecomm/views/admin/auth/signup.js
@@ -1,13 +1,17 @@
+const layout = require("../layout");
+
 module.exports = ({ req }) => {
-  return `
-    <div>
-      Your id is: ${req.session.userId}
-      <form method="POST">
-        <input placeholder="email" name="email" />
-        <input placeholder="password" name="password" />
-        <input placeholder="password confirmation" name="passwordConfirmation" />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `;
+  return layout({
+    content: `
+      <div>
+        Your id is: ${req.session.userId}
+        <form method="POST">
+          <input placeholder="email" name="email" />
+          <input placeholder="password" name="password" />
+          <input placeholder="password confirmation" name="passwordConfirmation" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+    `
+  });
 };
