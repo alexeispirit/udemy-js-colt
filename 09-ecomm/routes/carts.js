856adf94a80a2305034b4d958fb03f51bf05ee83
alexeispirit <alexeispirit@gmail.com>
2019-11-28 11:04:12 +0500

    397-submission-options

--- 09-ecomm/routes/carts.js
+++ 09-ecomm/routes/carts.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+router.post("/cart/products", (req, res) => {
+  res.send("product added");
+});
+
 module.exports = router;
