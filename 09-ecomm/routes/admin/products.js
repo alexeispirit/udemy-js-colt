6fbaca13e6f9de8f5ccf594c83b9f474f3c4fc7a
alexeispirit <alexeispirit@gmail.com>
2019-11-28 09:01:20 +0500

    380-requireing-authentication

--- 09-ecomm/routes/admin/products.js
+++ 09-ecomm/routes/admin/products.js
@@ -5,23 +5,24 @@ const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const productsIndexTemplate = require("../../views/admin/products/index");
 const { requireTitle, requirePrice } = require("./validators");
-const { handleErrors } = require("./middlewares");
+const { handleErrors, requireAuth } = require("./middlewares");
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get("/admin/products", async (req, res) => {
+router.get("/admin/products", requireAuth, async (req, res) => {
   const products = await productsRepo.getAll();
 
   res.send(productsIndexTemplate({ products }));
 });
 
-router.get("/admin/products/new", (req, res) => {
+router.get("/admin/products/new", requireAuth, (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
 router.post(
   "/admin/products/new",
+  requireAuth,
   upload.single("image"),
   [requireTitle, requirePrice],
   handleErrors(productsNewTemplate),
