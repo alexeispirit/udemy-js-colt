6dc1e3ce21fed839b237fe0ecb32f0adedf0aea9
alexeispirit <alexeispirit@gmail.com>
2019-12-05 13:54:41 +0500

    479-fixing-test

--- 12-tme/samplewebproject/test/app.test.js
+++ 12-tme/samplewebproject/test/app.test.js
@@ -18,5 +18,19 @@ it("shows a success message with a valid email", async () => {
 
   const h1 = dom.window.document.querySelector("h1");
 
-  console.log(h1.innerHTML);
+  assert.strictEqual(h1.innerText, "Looks good!");
+});
+
+it("shows a fail message with a invalid email", async () => {
+  const dom = await render("index.html");
+  const input = dom.window.document.querySelector("input");
+
+  input.value = "asfsdfsf";
+  dom.window.document
+    .querySelector("form")
+    .dispatchEvent(new dom.window.Event("submit"));
+
+  const h1 = dom.window.document.querySelector("h1");
+
+  assert.strictEqual(h1.innerText, "Invalid email");
 });
