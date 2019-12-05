226554a4dd5f036cc9a5540234eb95f5e5e57178
alexeispirit <alexeispirit@gmail.com>
2019-12-05 13:03:38 +0500

    467-adding-colors

--- 12-tme/runner.js
+++ 12-tme/runner.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const chalk = require("chalk");
 
 class Runner {
   constructor() {
@@ -8,6 +9,7 @@ class Runner {
 
   async runTests() {
     for (let file of this.testFiles) {
+      console.log(chalk.gray(`---- ${file.shortName}`));
       const beforeEaches = [];
 
       global.beforeEach = fn => {
@@ -18,17 +20,17 @@ class Runner {
         beforeEaches.forEach(func => func());
         try {
           fn();
-          console.log(`OK - ${desc}`);
+          console.log(chalk.green(`OK - ${desc}`));
         } catch (err) {
-          console.log(`X - ${desc}`);
-          console.log("\t", err.message);
+          console.log(chalk.red(`X - ${desc}`));
+          console.log(chalk.red("\t", err.message));
         }
       };
 
       try {
         require(file.name);
       } catch (err) {
-        console.log(err);
+        console.log(chalk.red(err));
       }
     }
   }
@@ -41,7 +43,7 @@ class Runner {
       const stats = await fs.promises.lstat(filepath);
 
       if (stats.isFile() && file.includes(".test.js")) {
-        this.testFiles.push({ name: filepath });
+        this.testFiles.push({ name: filepath, shortName: file });
       } else if (stats.isDirectory()) {
         const childFiles = await fs.promises.readdir(filepath);
         files.push(...childFiles.map(f => path.join(file, f)));
