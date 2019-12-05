84cfd4c98074d32e9ac03aa7533c366f8d829c7d
alexeispirit <alexeispirit@gmail.com>
2019-12-05 11:24:32 +0500

    453-fake-DOM-events

--- 11-movie-testing/test/autocomplete.test.js
+++ 11-movie-testing/test/autocomplete.test.js
@@ -1,4 +1,5 @@
-it("shows an autocomplete", () => {
+beforeEach(() => {
+  document.querySelector("#target").innerHTML = "";
   createAutoComplete({
     root: document.querySelector("#target"),
     fetchData() {
@@ -12,8 +13,16 @@ it("shows an autocomplete", () => {
       return movie.Title;
     }
   });
+});
+
+it("dropdown starts closed", () => {});
+
+it("after searching dropdown opens up", () => {
+  const input = document.querySelector("input");
+  input.value = "avengers";
+  input.dispatchEvent(new Event("input"));
 
   const dropdown = document.querySelector(".dropdown");
 
-  expect(dropdown.className).not.to.include("is-active");
+  expect(dropdown.className).to.include("is-active");
 });
